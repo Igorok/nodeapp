678df36bc437858bc7f6c018ccdb108146a1561b
Igor <Dubinin>
2020-04-26 18:21:13 +0300

    train sort and decorator

--- training.js
+++ training.js
@@ -73,7 +73,6 @@ Insertion sort
 Алгоритм сортировки, в котором элементы входной последовательности просматриваются по одному, и каждый новый поступивший элемент размещается в подходящее место среди ранее упорядоченных элементов
 */
 
-const sortArray = [54,26,93,17,77,31,44,55,20, 94, 94];
 const sortInsertion = (arr) => {
     for (let i = 1; i < arr.length; i++) {
         const temp = arr[i];
@@ -91,3 +90,138 @@ const sortInsertion = (arr) => {
 
 // [17, 20, 26, 31, 44, 54, 55, 77, 93, 94, 94]
 // sortInsertion(sortArray);
+
+
+/*
+merge sort
+
+Сортируемый массив разбивается на две части примерно одинакового размера;
+Каждая из получившихся частей сортируется отдельно, например — тем же самым алгоритмом;
+Два упорядоченных массива половинного размера соединяются в один.
+
+*/
+
+const sortMerge = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const center = Math.ceil(arr.length / 2);
+    const left = sortMerge(arr.slice(0, center));
+    const right = sortMerge(arr.slice(center, arr.length));
+
+    let i = 0;
+    let j = 0;
+    let newArr = [];
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            newArr.push(left[i]);
+            i = i + 1;
+        } else {
+            newArr.push(right[j]);
+            j = j + 1;
+        }
+    }
+
+    if (i != left.length) {
+        newArr = newArr.concat(left.slice(i, left.length));
+    } else {
+        newArr = newArr.concat(right.slice(j, right.length));
+    }
+
+    return newArr;
+};
+
+// [ 17, 20, 26, 31, 44, 54, 55, 77, 93, 94, 94 ]
+// console.log('sortMerge(sortArray)', sortMerge(sortArray));
+
+/*
+quick sort
+
+ - Выбрать из массива элемент, называемый опорным. Это может быть любой из элементов массива. От выбора опорного элемента не зависит корректность алгоритма, но в отдельных случаях может сильно зависеть его эффективность (см. ниже).
+ - Сравнить все остальные элементы с опорным и переставить их в массиве так, чтобы разбить массив на три непрерывных отрезка, следующих друг за другом: «элементы меньшие опорного», «равные» и «большие»[1].
+ - Для отрезков «меньших» и «больших» значений выполнить рекурсивно ту же последовательность операций, если длина отрезка больше единицы.
+
+*/
+
+const sortQuick = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const start = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < start) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    left.push(start);
+
+    arr = sortQuick(left).concat(sortQuick(right));
+
+    return arr;
+};
+
+// [ 17, 20, 26, 31, 44, 54, 55, 77, 93, 94, 94 ]
+// console.log('sortQuick(sortArray)', sortQuick(sortArray));
+
+/*
+
+Замыкание – это функция, которая запоминает свои внешние переменные и может получить к ним доступ. В некоторых языках это невозможно, или функция должна быть написана специальным образом, чтобы получилось замыкание. Но, как было описано выше, в JavaScript, все функции изначально являются замыканиями (есть только одно исключение, про которое будет рассказано в Синтаксис "new Function").
+
+То есть, они автоматически запоминают, где были созданы, с помощью скрытого свойства [[Environment]] и все они могут получить доступ к внешним переменным.
+
+
+Декоратор – это обёртка вокруг функции, которая изменяет поведение последней. Основная работа по-прежнему выполняется функцией.
+
+const abArr = [
+    [1,2], [3,4], [5,6], [5,6], [3,4], [1,2]
+];
+
+let slowAb = async (a, b) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res(a + b);
+        }, 3 * 1000);
+    });
+};
+
+const worker = async () => {
+    for (const arr of abArr) {
+        console.time();
+        console.log('slowAb', ...arr, await slowAb(...arr));
+        console.timeEnd();
+    }
+};
+
+// worker();
+
+const cacheAb = (func) => {
+    const cache = {};
+    return async (...args) => {
+        const a = args[0];
+        const b = args[1];
+        const k = `${a}_${b}`;
+
+        if (! cache[k]) {
+            cache[k] = await func(...args);
+        }
+
+        return cache[k];
+    };
+}
+
+slowAb = cacheAb(slowAb);
+
+worker();
+
+*/
+
+
+
