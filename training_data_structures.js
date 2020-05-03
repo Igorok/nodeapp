8aaaf55b5150c414f4caeaaf5b1547d35cc23c9d
Igor <Dubinin>
2020-05-03 20:14:58 +0300

    training data structures

--- training_data_structures.js
+++ training_data_structures.js
@@ -206,3 +206,210 @@ class BinaryTreeNode {
     }
 }
 
+
+/*
+
+Граф — абстрактный математический объект, представляющий собой множество вершин графа и набор рёбер, то есть соединений между парами вершин. Например, за множество вершин можно взять множество аэропортов, обслуживаемых некоторой авиакомпанией, а за множество рёбер взять регулярные рейсы этой авиакомпании между городами.
+
+Вершина (иногда её называют узел) - основная часть графа. Может иметь имя, которое называется ключ. Также вершина может обладать дополнительной информацией, которую мы будем называть полезной нагрузкой.
+
+Ребро (или дуга) - другая фундаментальная часть графа. Ребро, соединяющее две вершины, показывает наличие между ними определённых отношений. Рёбра могут быть одно- и двунаправленными. Если все рёбра графа однонаправленные, то мы называем его направленным графом.
+
+Вес - рёбра могут иметь вес, показывающий стоимость перемещения от одной вершины к другой. Например, в графе дорог, связывающих города, вес ребра может отображать расстояние между двумя населёнными пунктами.
+
+Путь в графе - это последовательность вершин, соединённых рёбрами.
+
+Цикл в направленном графе начинается и заканчивается в одной и той же вершине.
+
+Есть две широко известные реализации графа: матрица смежности и список смежности.
+
+Одним из простейших способов реализовать граф является использование двумерной матрицы. Таблица осями которой являются вершины графа, если они связаны то значением являются ребра.
+
+Более пространственно-экономичным способом реализации разреженного графа является использование списка смежности. Список, внутри которого списки содержащие связаные вершины.
+
+*/
+
+// вершина
+class Vertex {
+    constructor(p) {
+        this.id = p.id;
+        this.connectedTo = {};
+        this.color = 'white';
+        this.parent = undefined;
+        this.distance = 0;
+    }
+
+    addNeighbor(nbr, weight = 0) {
+        this.connectedTo[nbr.id] = weight;
+    }
+}
+
+// граф
+class Graph {
+    constructor() {
+        this.vertices = {};
+        this.numVertices = 0;
+    }
+
+    // вершина
+    addVertex(value) {
+        this.numVertices = this.numVertices + 1;
+        const nv = new Vertex({
+            id: value,
+        });
+        this.vertices[value] = nv;
+        return nv;
+    }
+
+    // ребро
+    addEdge(from, to, weight = 0) {
+        if (from.id === to.id) {
+            return;
+        }
+        from.addNeighbor(to, weight);
+        to.addNeighbor(from, weight);
+    }
+
+    // breadth first search - проходит по всем дочерним элементам, потом по их потомкам, используется очередь
+    bfs(from, to) {
+        const queue = [ from ];
+        let search = undefined;
+
+        while (queue.length && ! search) {
+            const val = queue.shift();
+            val.color = 'grey';
+
+            for (const k in val.connectedTo) {
+                const v = this.vertices[k];
+                if (v.color !== 'white') {
+                    continue;
+                }
+                v.distance = val.distance + 1;
+                v.parent = val;
+
+                if (v.id === to.id) {
+                    search = v;
+                    break;
+                }
+
+                queue.push(v);
+            }
+
+            val.color = 'black';
+        }
+
+        return to;
+    }
+
+    // deep first search - проходит по всем потомкам одной вершины, потом переходит к соседям, используется стек
+    dfs(from, to) {
+        const stack = [ from ];
+        let search = undefined;
+
+        while (stack.length && ! search) {
+            const val = stack.pop();
+            val.color = 'grey';
+
+            for (const k in val.connectedTo) {
+                const v = this.vertices[k];
+                if (v.color !== 'white') {
+                    continue;
+                }
+                v.distance = val.distance + 1;
+                v.parent = val;
+
+                if (v.id === to.id) {
+                    search = v;
+                    break;
+                }
+
+                stack.push(v);
+            }
+
+            val.color = 'black';
+        }
+
+        return to;
+    }
+
+
+    // traverse
+    traverse(from) {
+        console.log('from', from.id, from.distance);
+        if (from.parent) {
+            this.traverse(from.parent);
+        }
+    }
+}
+
+// задача - найти путь от одного слова до другого, меняя 1 букву
+let words = [
+    "FOOL", "POOL", "POLL", "POLE",
+    "PALE", "SALE", "SAGE", "POPE",
+    "ROPE", "NOPE", "PIPE", "PAPE",
+    "POLE", "PORE", "FAIL", "FALL",
+    "PALL", "COOL", "PAGE", "HOPE",
+    "FOUL", "FOIL"
+];
+
+// группируем слова, создаем ключ меняя одну букву на _ и помещаем в массив все слова подходящие под этот ключ
+const groupWords = (words) => {
+    const groupedWords = {};
+    for (const word of words) {
+        for (let i = 0; i < 4; i++) {
+            let key = word.substring(0, i) + '_';
+            if (i !== 3) {
+                key += word.substring(i + 1, 4);
+            }
+
+            if (! groupedWords[key]) {
+                groupedWords[key] = [];
+            }
+
+            groupedWords[key].push(word);
+        };
+    }
+
+    return groupedWords;
+};
+
+// наполняем класс
+const buildWordsGraph = () => {
+    const graph = new Graph();
+    const groupedWords = groupWords(words);
+
+    // добавляем в граф вершины
+    for (const word of words) {
+        graph.addVertex(word);
+    }
+
+    // проходимся по сгруиированным словам и связываем вершины
+    for (const k in groupedWords) {
+        const arr = groupedWords[k];
+        if (arr.length <= 1) {
+            continue;
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            const v1 =  graph.vertices[arr[i]];
+            for (let j = 0; j < arr.length; j++) {
+                const v2 =  graph.vertices[arr[j]];
+                graph.addEdge(v1, v2);
+            }
+        }
+    }
+
+    return graph;
+};
+
+// проходимся в глубину и ширину чтобы найти путь
+
+// console.log('bfs');
+// let graph = buildWordsGraph();
+// const bfs = graph.bfs(graph.vertices['FOOL'], graph.vertices['FALL']);
+// graph.traverse(bfs);
+
+// console.log('dfs');
+// graph = buildWordsGraph();
+// const dfs = graph.dfs(graph.vertices['FOOL'], graph.vertices['FALL']);
+// graph.traverse(dfs);
